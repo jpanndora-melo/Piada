372ae79b7813f885f42c273155e3d0fa39702103
Jessica <jpanndora@gmail.com>
2025-09-19 01:25:29 -0300

    Atividade Concluída

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sempre o Joãozinho");
+Console.WriteLine();
+Console.WriteLine("Na aula de matemática, a professora pergunta:");
+Console.WriteLine("- Joãozinho, se tenho 6 laranjas em uma mão e 5 na outra, o que tenho no total?");
+Console.WriteLine("Joãozinho responde:");
+Console.WriteLine("- Mãos grandes, professora!");
